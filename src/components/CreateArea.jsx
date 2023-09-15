f26f3a13d50dc8237a2f8e5561e2ce9a68cf35df
CodeSandbox <support@codesandbox.io>
2023-09-15 11:18:09 +0000

    Sandbox snapshot commit

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const titleInputRef = useRef(null);
+
+  useEffect(() => {
+    if (isExpanded) {
+      titleInputRef.current.focus();
+    }
+  }, [isExpanded]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -26,23 +38,36 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setIsExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+        {isExpanded && (
+          <input
+            ref={titleInputRef}
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
+          onClick={expand}
+          onFocus={expand}
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
