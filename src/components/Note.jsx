13bf0d85d65a9a4da1dc9fe47f098202f47c8066
Muhammad Ahmad <muhammadahmad522@gmail.com>
2023-09-21 12:24:27 +0000

    time updated automatically

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,20 +1,58 @@
-import React from "react";
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Collapse from "@mui/material/Collapse";
+
+// Import the DynamicTimeDisplay component
+import DynamicTimeDisplay from "./DynamicTimeDisplay";
 
 function Note(props) {
-  function handleClick() {
+  console.log(props.timestamp);
+  const [expanded, setExpanded] = useState(false);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
+
+  function handleDeleteClick() {
     props.onDelete(props.id);
   }
 
+  const currentDate = new Date().toLocaleDateString();
+
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <IconButton onClick={handleClick}>
-        <DeleteIcon />
-      </IconButton>
-    </div>
+    <Card className="note">
+      <CardHeader
+        title={props.title}
+        subheader={<DynamicTimeDisplay timestamp={props.timestamp} />} // Use DynamicTimeDisplay here
+        action={
+          <IconButton onClick={handleExpandClick} aria-expanded={expanded}>
+            <ExpandMoreIcon
+              sx={{
+                transform: expanded ? "rotate(180deg)" : "rotate(0deg)",
+                transition: "transform 0.2s",
+              }}
+            />
+          </IconButton>
+        }
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {expanded ? props.content : `${props.content.substring(0, 10)}...`}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton onClick={handleDeleteClick} aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 }
 
